6f5ea97646a1ed7dcb9813c7f38b956c8d9f41db
Henrique Dias Militão <henriquemilitao35@gmail.com>
2025-10-02 10:25:28 -0400

    fix: controle semanal com livre tbm

--- src/components/2.0/Semanas/components/listagemPorData.tsx
+++ src/components/2.0/Semanas/components/listagemPorData.tsx
@@ -100,7 +100,7 @@ export function ListagemPorCategoria({
               <div className="mb-3">
                 <div className="flex justify-between items-center mb-1">
                   <h2 className="text-base font-bold text-gray-700">
-                    {categoria}
+                    {categoria === 'Aleatório' ? 'Livre' : categoria}
                   </h2>
                   <span className="text-sm text-gray-600">
                     {formatCurrencyFromCents(gastoNaSemana)} /{" "}
